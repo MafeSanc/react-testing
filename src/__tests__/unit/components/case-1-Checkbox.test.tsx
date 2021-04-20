b592277ae5f8daa2961820cd52294cedb2c5e6ec
Mafe Sanchez <mafesanchez0326@gmail.com>
2021-04-20 10:32:39 +0100

    case-1, passed 4 tests

--- src/__tests__/unit/components/case-1-Checkbox.test.tsx
+++ src/__tests__/unit/components/case-1-Checkbox.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { axe } from 'jest-axe'
 import { render, fireEvent } from '@testing-library/react'
-
+import ReactDOM from 'react-dom'
 import Checkbox from '../../../components/Checkbox'
 
 /**
@@ -10,14 +10,69 @@ import Checkbox from '../../../components/Checkbox'
  * make sure it still works as a regular checkbox
  * should.
  */
+
 describe('The <Checkbox /> component', () => {
-  it('❌ Should render the label and checkbox the user will see', () => {})
+  const defaultCheckboxProps = {
+    label: 'TEST_CHECKBOX_LABEL',
+    id: 'TEST_CHECKBOX_ID',
+    background: '#000',
+    checkMarkBackground: '#fff',
+    onChange: jest.fn(),
+    checked: false,
+  }
 
-  it('❌ Should make the checkbox accessible by setting the id and htmlFor attributes on label and checkbox', () => {})
+  //this const is meant to avoid repetition
+  const setupCheckbox = (props = defaultCheckboxProps) => render(<Checkbox {...props} />)
 
-  it('❌ Should call the onChange handler when it is provided', () => {})
+  it('❌ Should render the label and checkbox the user will see', () => {
+    // const container = document.createElement('div')
+    // ReactDOM.render(
+    //   <Checkbox {...defaultCheckboxProps} />,
+    //   container
+    // )
 
-  it('❌ Should change state correctly when clicked (checked and unchecked)', () => {})
+    // expect(container.querySelector('label')).not.toBeNull()
+    // expect(container.querySelector('input[type="checkbox"]')).not.toBeNull()
+
+    // const{ getByLabelText, debug} = render(
+    //   <Checkbox {...defaultCheckboxProps}  />,
+    // )
+    // debug(getByLabelText('TEST_CHECKBOX_LABEL'))
+
+    const { asFragment } = render(<Checkbox {...defaultCheckboxProps} />)
+    // console.log(asFragment())
+    expect(asFragment()).toMatchInlineSnapshot()
+  })
+
+  it(' Should make the checkbox accessible by setting the id and htmlFor attributes on label and checkbox', () => {
+    const { getByLabelText, debug } = render(
+      <Checkbox {...defaultCheckboxProps} />,
+    )
+
+    expect(getByLabelText('TEST_CHECKBOX_LABEL')).toBeInTheDocument()
+  })
+
+
+  it('❌ Should call the onChange handler when it is provided', () => {
+    const { getByLabelText } = setupCheckbox()
+
+    const checkbox = getByLabelText(defaultCheckboxProps.label)
+
+    fireEvent.click(checkbox)
+
+    expect(defaultCheckboxProps.onChange).toHaveBeenCalled()
+  })
+
+
+  it('❌ Should change state correctly when clicked (checked and unchecked)', () => {
+    const { getByLabelText } = setupCheckbox({
+      ...defaultCheckboxProps,
+      checked: true
+    })
+
+    expect(getByLabelText(defaultCheckboxProps.label)).toBeChecked()
+
+  })
 
   it('❌ should not fail any accessibility tests', async () => {})
 })
